6d972e7879faf4f802b3a663de86080022210d66
slim5adamno <vipsforeveralways@gmail.com>
2021-02-01 22:09:20 +0530

    Connect to heroku..if not work revert

--- admin/connection.php
+++ admin/connection.php
@@ -1,8 +1,8 @@
 <?php
-   $host        = "host = localhost";
+   $host        = "host = ec2-52-70-135-246.compute-1.amazonaws.com";
    $port        = "port = 5432";
-   $dbname      = "dbname = timetable";
-   $credentials = "user = postgres password=1221";
+   $dbname      = "dbname = d7r8m2mvtjpn0l";
+   $credentials = "user = gngswawgmaejmu password=ef43dbb51f3ea72de9839887cd9f618ba6013cfb75577f80cd5093b52a2803c7";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
