8997bf02c5ab56775c0339d4c4c045d72f16773e
vipul <vipul9aug@gmail.com>
2021-03-04 13:45:39 +0530

    Fixed gen-time-table-1.php , added condition to stream such that did accepts same name

--- admin/gen-time-table-1.php
+++ admin/gen-time-table-1.php
@@ -96,7 +96,7 @@ else{
                                                         // $sql="select sname from subjects where did=(select did from department where name='$_SESSION[dept]') and semester='$_SESSION[sem]'";
                                                        // $sql="select sname from subjects inner join course on subjects.cno=course.cno inner join department on course.did=department.did and course.ctype='$_SESSION[ctype]' and subjects.semester='$_SESSION[sem]'and department.name='$_SESSION[dept]' and subjects.stype='$_SESSION[stype]'";
                                                        // $sql="select sname from subjects where semester='$_SESSION[sem]' and stype='$_SESSION[stype]' and cno in(select cno from course where ctype='$_SESSION[ctype]' and did=(select did from department where name='$_SESSION[dept]'))";
-                                                        $sql="select sname from subjects where semester='$_SESSION[sem]' and stype='$_SESSION[stype]' and year='$_SESSION[year]' and did=(select did from department where name='$_SESSION[dept]')";
+                                                        $sql="select sname from subjects where semester='$_SESSION[sem]' and stype='$_SESSION[stype]' and year='$_SESSION[year]' and did=(select did from department where name='$_SESSION[dept]' and stream='$_SESSION[stream]' and ctype='$_SESSION[ctype]')";
                                                         $ret=pg_query($db,$sql);
                                                         pg_last_error($db);
                                                         if(!$ret) {
